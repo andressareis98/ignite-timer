5b4c2f5c1f6df128e51f1b649e440f8b5d7e13c7
Andressa Reis Araujo <andressareis98@outlook.com>
2023-06-15 11:15:32 -0300

    validação de campos e exibição de mensagens de erros com a biblioteca zod

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,7 @@
 import { Play } from "phosphor-react";
 import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as zod from "zod";
 
 import {
   CountdownContainer,
@@ -11,12 +13,21 @@ import {
   TaskInput,
 } from "./styles";
 
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, "Informe a tarefa"),
+  minutesAmount: zod.number().min(5).max(60),
+});
+
 export function Home() {
-  const { register, handleSubmit, watch } = useForm();
+  const { register, handleSubmit, watch } = useForm({
+    resolver: zodResolver(newCycleFormValidationSchema),
+  });
 
   function handleCreateNewCycle(data: any) {
     console.log(data);
   }
+
+  /* console.log(formState.errors); Para que fosse possivel usar o formState.errors, o formState deve ser importado em useForm */
 
   const task = watch("task");
   const isSubmitDisabled = !task;
